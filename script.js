e0748a28e3f2595ca71cabe43d3aebce494194d4
adhithiyan <adhithiyan.work@gmail.com>
2023-09-09 15:25:23 +0530

    add: win,lose and tie conditions

--- script.js
+++ script.js
@@ -39,8 +39,15 @@ const screenController = function(doc){
         })
     }
 
+    const winLoseDisplay = function(winner, val){
+        const div = document.querySelector(".winlose")
+
+        div.textContent = `The winner is ${winner}(${val})`
+    }
+
     return {
-        display
+        display,
+        winLoseDisplay
     }
 }(document);
 
@@ -64,9 +71,11 @@ const getPostion = function(doc){
                 if(!board[row][col]){
                     gameBoard.update(row , col , game.activePlayer().value())
                     screenController.display()
-                    console.log(game.activePlayer().name())
+                    game.winLose()
+
+                    // console.log(game.activePlayer().name())
                     game.switchPlayers()
-                    console.log(board)
+                    // console.log(board)
                 }
             })
         }
@@ -90,6 +99,7 @@ const Player = function(fname , fvalue){
 
 /* controls the flow of the game */
 const game = function(){
+    let board = gameBoard.board()
     const player1 = Player("john" , "o")
     const player2 = Player("Max" , "x")
     let currentPlayer = player2
@@ -100,11 +110,41 @@ const game = function(){
 
     const activePlayer = () => currentPlayer
 
+    const winLose = function(){
+        
+        let tieCount = 0
+        // to check for tie
+        board.forEach((e) => {
+            e.forEach(box => {
+                if (box != ""){
+                    tieCount++
+                } 
+            })
+        })
+        if(tieCount == 9){
+            screenController.winLoseDisplay("no one", "tie")
+        }
+        for(let i = 0; i < 3; i++){
+            if(board[0][i] != 0 && board[1][i] != 0 && board[2][i] != 0 || board[i][0] != 0 && board[i][1] != 0 && board[i][2] != 0){
+                //check for single row and column win
+                if(board[0][i] == board[1][i] && board[1][i] == board[2][i] || board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+                    screenController.winLoseDisplay(activePlayer().name() , activePlayer().value())
+                }
+                //checks for diagonal win
+                else if(board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+                    screenController.winLoseDisplay(activePlayer().name() , activePlayer().value())
+                } 
+            }
+        }   
+        return "lost"
+    }
+
     return{
         switchPlayers,
-        activePlayer
+        activePlayer,
+        winLose
     } 
 
 }();
 
-screenController.display()  
+screenController.display()
